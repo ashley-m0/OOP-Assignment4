757890db528edd9ba7df0aafb7330f4028ed3bbb
ashley-m0 <ashley.mojica@knights.ucf.edu>
2021-07-04 11:28:12 -0400

    Update 3 (7/4)

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -1,15 +1,23 @@
 package ucf.assignments;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App extends Application {
 
+    static AnchorPane root;
+    static List<GridPane> grid = new ArrayList<GridPane>();
+    static int currIndex = 0;
     public static void main(String[] args) {
         launch(args);
     }
@@ -17,15 +25,24 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+            root = (AnchorPane)FXMLLoader.load(getClass().getResource("Anchor.fxml"));
+
+            grid.add((GridPane)FXMLLoader.load(getClass().getResource("App.fxml")));
+            grid.add((GridPane)FXMLLoader.load(getClass().getResource("Menu.fxml")));
+            root.getChildren().add(grid.get(0));
 
             Scene scene = new Scene(root);
-
             primaryStage.setScene(scene);
-            primaryStage.setTitle("List");
+            primaryStage.setTitle("ToDo");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void setPane (int index){
+        root.getChildren().remove(grid.get(currIndex));
+        root.getChildren().add(grid.get(index));
+        currIndex = index;
     }
 }
--- src/main/java/ucf/assignments/AppController.java
+++ src/main/java/ucf/assignments/AppController.java
@@ -4,4 +4,13 @@ import javafx.event.ActionEvent;
 
 
 public class AppController {
+    public void submitTaskCompletedButton(ActionEvent actionEvent) {
+    }
+
+    public void menuButtonPressed(ActionEvent actionEvent) {
+        App.setPane(1);
+    }
+
+    public void changeView(ActionEvent actionEvent) {
+    }
 }
