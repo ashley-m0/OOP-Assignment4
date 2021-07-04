c94f39b5bc19c7f1cb59f3834277edf75831ceb5
ashley-m0 <ashley.mojica@knights.ucf.edu>
2021-07-04 12:04:55 -0400

    Update 5 (7/4)

--- src/main/java/ucf/assignments/MenuController.java
+++ src/main/java/ucf/assignments/MenuController.java
@@ -1,4 +1,51 @@
 package ucf.assignments;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+
 public class MenuController {
+    public void editTitleChecked(ActionEvent actionEvent) {
+    }
+
+    public void updateListButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void removeTaskChecked(ActionEvent actionEvent) {
+    }
+
+    public void addListButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void editDescriptionChecked(ActionEvent actionEvent) {
+    }
+
+    public void editDateChecked(ActionEvent actionEvent) {
+    }
+
+    public void newDate(ActionEvent actionEvent) {
+    }
+
+    public void updateTaskButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void addTaskButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void scanListButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void singleListInsertedChecked(ActionEvent actionEvent) {
+    }
+
+    public void multipleListInsertedChecked(ActionEvent actionEvent) {
+    }
+
+    public void saveOneListChecked(ActionEvent actionEvent) {
+    }
+
+    public void saveAllLIstChecked(ActionEvent actionEvent) {
+    }
+
+    public void saveListButtonPressed(ActionEvent actionEvent) {
+    }
 }
