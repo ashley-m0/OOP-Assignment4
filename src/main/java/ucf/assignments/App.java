4ed881baa78b3a1c02c6436c7179430216f16f0b
ashley-m0 <ashley.mojica@knights.ucf.edu>
2021-07-04 11:50:18 -0400

    Update 4 (7/4)

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -5,19 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class App extends Application {
 
-    static AnchorPane root;
-    static List<GridPane> grid = new ArrayList<GridPane>();
-    static int currIndex = 0;
     public static void main(String[] args) {
         launch(args);
     }
@@ -25,11 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            root = (AnchorPane)FXMLLoader.load(getClass().getResource("Anchor.fxml"));
-
-            grid.add((GridPane)FXMLLoader.load(getClass().getResource("App.fxml")));
-            grid.add((GridPane)FXMLLoader.load(getClass().getResource("Menu.fxml")));
-            root.getChildren().add(grid.get(0));
+            Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
 
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
@@ -40,9 +29,4 @@ public class App extends Application {
         }
     }
 
-    public static void setPane (int index){
-        root.getChildren().remove(grid.get(currIndex));
-        root.getChildren().add(grid.get(index));
-        currIndex = index;
-    }
 }
--- src/main/java/ucf/assignments/AppController.java
+++ src/main/java/ucf/assignments/AppController.java
@@ -1,14 +1,26 @@
 package ucf.assignments;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class AppController {
     public void submitTaskCompletedButton(ActionEvent actionEvent) {
     }
 
-    public void menuButtonPressed(ActionEvent actionEvent) {
-        App.setPane(1);
+    public void menuButtonPressed(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Scene menuScene = new Scene(root);
+
+        Stage window = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(menuScene);
+        window.show();
     }
 
     public void changeView(ActionEvent actionEvent) {
